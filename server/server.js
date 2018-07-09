89afaebc7e1f18a4620c284feb363d0403868446
Imran Sayed <imranhsayed@gmail.com>
2018-07-09 09:51:23 +0530

    Add the get request with id

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ let bodyParser = require( 'body-parser' );
 let {mongoose} = require( './db/mongoose' );
 let {Todo} = require( './models/todo' );
 let {User} = require( './models/user' );
+let {ObjectID} = require( 'mongodb' );
 
 // Create a server using express js.
 let app = express();
@@ -63,6 +64,25 @@ app.get( '/todos', ( req, res ) => {
 		res.status( 400 ).send(error);
 	} )
 } );
+
+/**
+ * Set the get route with an ID.
+ * :id will create an id variable which will be available on the req object in req.params.id
+ */
+app.get( '/todos/:id', ( req, res ) => {
+	let id = req.params.id;
+	if ( ! ObjectID.isValid( id ) ) {
+		return res.status( 404 ).send();
+	}
+	Todo.findById( id ).then( ( todo ) => {
+		if ( ! todo ) {
+			return res.status( 404 ).send();
+		}
+		res.send( {todo} );
+	}).catch( ( err ) => {
+		res.status( 400 ).send();
+	} )
+});
 
 // Export the app so that its available to other file when they require this file.
 module.exports = {app};
