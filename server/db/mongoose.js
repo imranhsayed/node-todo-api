56fd572f3fb4a3ca7c6b2331f7f7d7134207f85a
Imran Sayed <imranhsayed@gmail.com>
2018-07-11 10:44:52 +0530

    Add mongodb configurations

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,9 +6,10 @@ mongoose.Promise = global.Promise;
 /**
  * Connect to the database
  * Here mongodb is the protocol,
+ * If the MONGODB_URI  for mLab MongoDB database from heroku is not available then it will use our localhost TodoApp MongoDB database
  * //localhost:27017 is site url at port 27017 and TodoApp is the database name.
  */
-mongoose.connect( 'mongodb://localhost:27017/TodoApp' );
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp' );
 
 /**
  * We are using module.exports and setting it equal to mongoose, so that it can be available in other file that requires this file.
